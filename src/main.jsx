3968d680e208e6eb52d7a678b3aefc478eaf6deb
Mike Tagg <mdtagg91@gmail.com>
2023-01-11 12:27:33 -0800

    changed render method in main.jsx
    ReactDOM.render is deprecated so I changed the .render to .create root and
    changed the import to ReactDOM/client

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+// ReactDOM.render(<AppRouter />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AppRouter />
+)
