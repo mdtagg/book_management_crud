4a4b2ec65a51ca677ba2eb366ff4b6cc7da74d36
Mike Tagg <mdtagg91@gmail.com>
2023-01-18 15:53:30 -0800

    react context was added for books state
    a context was added in the new context folder and used in the AppRouter
    component. This was done so that the books state can be passed to the various
    compoents

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,20 +3,25 @@ import { BrowserRouter,Switch,Route } from 'react-router-dom'
 import Header from '../components/Header'
 import BookList from '../components/BookList'
 import UseLocalStorage from "../Hooks/UseLocalStorage";
+import BooksContext from "../context/BooksContext";
+import AddBook from "../components/AddBook";
 
 const AppRouter = () => {
     const [ books,setBooks ] = UseLocalStorage('books',[])
     console.log(books)
      return (
         <BrowserRouter>
+        <div>
             <Header />
             <div className='main'>
-                <Switch>
-                    <Route path='/' component={BookList} exact>
-
-                    </Route>
-                </Switch>
+                <BooksContext.Provider value={(books,setBooks)}>
+                    <Switch>
+                        <Route path='/' component={BookList} exact />
+                        <Route path='add' component={AddBook} />
+                    </Switch>
+                </BooksContext.Provider>
             </div>
+        </div>
         </BrowserRouter>
     )
 }
