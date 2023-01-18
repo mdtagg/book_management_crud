57bf86e5da7c69b09d8b39acd440ba0a19de1b55
Mike Tagg <mdtagg91@gmail.com>
2023-01-18 15:35:31 -0800

    UseLocalStorage hook added
    UseLocalStorage is checking to see if there is a value with the key books
    stored. If there is value is set to the books that are already in local
    storage. If not the state of value is set the books key and an empty array
    with useEffect

---
+++ src/Hooks/UseLocalStorage.jsx
@@ -0,0 +1,20 @@
+// import React from "react";
+import { useEffect,useState } from "react";
+
+const UseLocalStorage = (key,initialValue) => {
+    const [ value, setValue ] = useState(() => {
+        try {
+            const localValue = window.localStorage.getItem(key)
+            return localValue ? JSON.parse(localValue) : initialValue
+        }catch(error) {
+            return initialValue
+        }
+    })
+
+    useEffect(() => {
+        window.localStorage.setItem(key,JSON.stringify(value))
+    },[key,value])
+    return [value, setValue]
+}
+
+export default UseLocalStorage
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter,Switch,Route } from 'react-router-dom'
 import Header from '../components/Header'
 import BookList from '../components/BookList'
+import UseLocalStorage from "../Hooks/UseLocalStorage";
 
 const AppRouter = () => {
-    // const [ book,setBook ] = UseLocalStorage('book',[])
+    const [ books,setBooks ] = UseLocalStorage('books',[])
+    console.log(books)
      return (
         <BrowserRouter>
             <Header />
