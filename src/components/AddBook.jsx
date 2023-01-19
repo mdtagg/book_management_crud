7ebffc3fe7565385c88ff1a8057039cc8945fee1
Mike Tagg <mdtagg91@gmail.com>
2023-01-19 11:04:38 -0800

    onSubmit changed to handleOnSubmit as BookForm attribute

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -3,13 +3,13 @@ import BookForm from "./BookForm";
 import BooksContext from "../context/BooksContext";
 
 const AddBook = () => {
-    // const { books,setBooks } = useContext(BooksContext)
+    const { books,setBooks } = useContext(BooksContext)
     function handleOnSubmit(book) {
-
+        console.log(book)
     }
     return (
         <React.Fragment>
-            <BookForm onSubmit={handleOnSubmit} />
+            <BookForm handleOnSubmit={handleOnSubmit} />
         </React.Fragment>
     )
 }
